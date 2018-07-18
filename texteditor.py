36554f1c56a3340ecc0557d5b175057d355e97a1
unknown <ayush-206>
2018-07-18 09:45:35 +0530

    editor is modified

--- texteditor.py
+++ texteditor.py
@@ -1,103 +1,119 @@
-
 from tkinter import *
-import tkinter.scrolledtext as ScrolledText 
 import os
-from tkinter.filedialog import *
-import tkinter.messagebox   
+import tkinter.filedialog
+import tkinter.messagebox
 
-  
-root=Tk()
-root.geometry('800x400')
+root = Tk()
 root.iconbitmap(r"C:\Users\Ayush\Desktop\sample\icons\icons\favicon.ico")
-root.title("Text Editor")
-textArea=ScrolledText.ScrolledText(root,width=1990,height=1000)
-text=Text(root,width=360,height=100)
-text.grid()
-menu=Menu(root)
-root.config(menu=menu)
 
-def new(event=None):
-    root.title("untitled")
+name = "TextEditor"
+root.title(name)
+file_name = None
+root.geometry('900x500')
+
+
+#functions
+# File Menu
+def new_file(event=None):
+    root.title("Untitled")
     global file_name
-    file_name=None
-    text.delete(1.0,END)
+    file_name = None
+    content_text.delete(1.0, END)
+    on_content_changed()
 
 
+def open_file(event=None):
+    input_file_name = tkinter.filedialog.askopenfilename(defaultextension=".txt",
+                                                         filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt"),
+                                                                    ("HTML", "*.html"), ("CSS", "*.css"),
+                                                                    ("JavaScript", "*.js")])
+    if input_file_name:
+        global file_name
+        file_name = input_file_name
+        root.title('{} - {}'.format(os.path.basename(file_name),name))
+        content_text.delete(1.0, END)
+        with open(file_name) as _file:
+            content_text.insert(1.0, _file.read())
 
-def open():
-    askopenfilename()
+    on_content_changed()
 
 
+def write_to_file(file_name):
+    try:
+        content = content_text.get(1.0, 'end')
+        with open(file_name, 'w') as the_file:
+            the_file.write(content)
+    except IOError:
+        pass
 
-def save():
 
-    filename = asksaveasfilename()
+def save_as(event=None):
+    input_file_name = tkinter.filedialog.asksaveasfilename(defaultextension=".txt",
+                                                           filetypes=[("All Files", "*.*"), ("Text Documents", "*.txt"),
+                                                                    
+                                                                     ("JavaScript", "*.js")])
+    if input_file_name:
+        global file_name
+        file_name = input_file_name
+        write_to_file(file_name)
+        root.title('{} - {}'.format(os.path.basename(file_name),name))
+    return "break"
 
-    if filename:
 
-        alltext = text.get(1.0, END)                      
+def save(event=None):
+    global file_name
+    if not file_name:
+        save_as()
+    else:
+        write_to_file(file_name)
+    return "break"
+#End of file menu
 
-        open(filename, 'w').write(alltext) 
-
-def Saveas(self, app, pad):
-        """
-        saves the contents in pad to file
-        to specified filename
-        """
-        save_file = asksaveasfilename(parent=app)
-        data = pad.get('1.0', GUI.END)[:-1]
-        f = open(save_file, 'w')
-        f.write(data)
-        f.close()
-        x = save_file
-        if platform.system() == 'Windows':
-            x = x.replace('/', '\\')
-            File.filename(map(str, x.split('\\'))[-1])
-        else:
-            pass
-        File.filepath(x)
-        app.title(File.name)
-
+# For Edit Menu
 def cut():
-
-    sel = text.get(SEL_FIRST, SEL_LAST)
-
-    text.delete(SEL_FIRST, SEL_LAST)
-
-
-
-
+    content_text.event_generate("<<Cut>>")
+    on_content_changed()
+    return "break"
 
 
 def copy():
+    content_text.event_generate("<<Copy>>")
+    on_content_changed()
+    return "break"
 
-    text.clipboard_clear()
-
-    text.clipboard_append(text.selection_get()) 
 
 def paste():
+    content_text.event_generate("<<Paste>>")
+    on_content_changed()
+    return "break"
 
-    try:
-
-        teext = text.selection_get(selection='CLIPBOARD')
-
-        text.insert(INSERT, teext)
-
-    except:
-
-        tkMessageBox.showerror("Errore","Gli appunti sono vuoti!")
 
 def undo():
-    if not stack:
-        return
-    previous = stack.pop()
-    previous.undo()
+    content_text.event_generate("<<Undo>>")
+    on_content_changed()
+    return "break"
 
-def help(event=None):
+
+def redo(event=None):
+    content_text.event_generate("<<Redo>>")
+    on_content_changed()
+    return "break"
+
+#End of Edit Menu
+# ABOUT MENU
+
+def display_help(event=None):
     tkinter.messagebox.showinfo(
-        "Help", "this is basically a note making pad made by python",
+        "Help", "this is a simple text editor like notepad",
         icon='question')
 
+
+def exit_editor(event=None):
+    
+        root.destroy()
+
+
+# Here, adding Line Numbers Functionality
 def get_line_numbers():
     output = ''
     if show_line_number.get():
@@ -105,6 +121,7 @@ def get_line_numbers():
         for i in range(1, int(row)):
             output += str(i) + '\n'
     return output
+
 
 def on_content_changed(event=None):
     update_line_numbers()
@@ -119,7 +136,7 @@ def update_line_numbers(event=None):
     line_number_bar.config(state='disabled')
 
 
-# Adding Cursor Functionality
+# Here ,Adding Cursor Functionality
 def show_cursor():
     show_cursor_info_checked = show_cursor_info.get()
     if show_cursor_info_checked:
@@ -134,7 +151,8 @@ def update_cursor(event=None):
     infotext = "Line: {0} | Column: {1}".format(line_num, col_num)
     cursor_info_bar.config(text=infotext)
 
-#Adding Text Highlight Functionality
+
+# Here, Adding Text Highlight Functionality
 def highlight_line(interval=100):
     content_text.tag_remove("active_line", 1.0, "end")
     content_text.tag_add(
@@ -152,7 +170,25 @@ def toggle_highlight(event=None):
     else:
         undo_highlight()
 
-# ICONS for the compound menu
+
+# Adding Change Theme Functionality
+def change_theme(event=None):
+    selected_theme = theme_choice.get()
+    fg_bg_colors = color_schemes.get(selected_theme)
+    foreground_color, background_color = fg_bg_colors.split('.')
+    content_text.config(
+        background=background_color, fg=foreground_color)
+
+
+# pop-up menu
+def show_popup_menu(event):
+    popup_menu.tk_popup(event.x_root, event.y_root)
+    
+    #End of About Menu
+    
+
+
+# Icons for the compound menu path
 new_file_icon = PhotoImage(file=r'C:\Users\Ayush\Desktop\sample\icons\icons\new_file.gif')
 open_file_icon = PhotoImage(file=r'C:\Users\Ayush\Desktop\sample\icons\icons\open_file.gif')
 cut_icon = PhotoImage(file=r'C:\Users\Ayush\Desktop\sample\icons\icons\cut.gif')
@@ -161,43 +197,120 @@ paste_icon = PhotoImage(file=r'C:\Users\Ayush\Desktop\sample\icons\icons\paste.g
 undo_icon = PhotoImage(file=r'C:\Users\Ayush\Desktop\sample\icons\icons\undo.gif')
 redo_icon = PhotoImage(file=r'C:\Users\Ayush\Desktop\sample\icons\icons\redo.gif')
 find_icon = PhotoImage(file=r'C:\Users\Ayush\Desktop\sample\icons\icons\find_text.gif')
+save_file_icon = PhotoImage(file=r'C:\Users\Ayush\Desktop\sample\icons\icons\find_text.gif')
+#Here File Menu codes Begains
+menu_bar = Menu(root) 
 
-#for file in the menu bar 
-fileMenu=Menu(menu,tearoff=0)
-menu.add_cascade(label="file",menu=fileMenu)
-fileMenu.add_command(label="new",accelerator='ctrl+N',compound='left',image=new_file_icon,command=new)
-fileMenu.add_command(label="open",accelerator='ctrl+O',compound='left',image=open_file_icon,command=open)
-fileMenu.add_command(label="save",accelerator='ctrl+S',compound='left',command=save)
-fileMenu.add_command(label="save as",accelerator='ctrl+sht+S',command=save)
-fileMenu.add_separator()
-fileMenu.add_command(label="Exit",command=root.destroy)
+file_menu = Menu(menu_bar, tearoff=0)
+file_menu.add_command(label='New', accelerator='Ctrl+N', compound='left', image=new_file_icon, underline=0,
+                      command=new_file)
+file_menu.add_command(label='Open', accelerator='Ctrl+O', compound='left', image=open_file_icon, underline=0,
+                      command=open_file)
+file_menu.add_command(label="Save", accelerator='Ctrl+S', compound='left', image=save_file_icon, underline=0,
+                      command=save)
+file_menu.add_command(label="Save As", accelerator='Ctrl+Shift+S', compound='left', underline=0, command=save_as)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", accelerator='Alt+F4', compound='left', underline=0, command=exit_editor)
+menu_bar.add_cascade(label='File', menu=file_menu)
+# End of File Menu
 
-#for open  in the menu bar
-EditMenu=Menu(menu,tearoff=0)
-menu.add_cascade(label="Edit",menu=EditMenu)
-EditMenu.add_command(label="undo",accelerator='ctrl+U',compound='left',image=undo_icon,command=new)
-EditMenu.add_command(label="cut",accelerator='ctrl+X',compound='left',image=cut_icon,command=cut)
-EditMenu.add_command(label="copy",accelerator='ctrl+C',compound='left',image=copy_icon,command=copy)
-EditMenu.add_command(label="paste",accelerator='ctrl+V',compound='left',image=paste_icon,command=paste)
-EditMenu.add_separator()
-EditMenu.add_command(label="Exit",command=root.destroy)
+edit_menu = Menu(menu_bar, tearoff=0)
+edit_menu.add_command(label='Undo', accelerator='Ctrl + Z', compound='left', image=undo_icon, underline=0, command=undo)
+edit_menu.add_command(label='Redo', accelerator='Ctrl+Y', compound='left', image=redo_icon, underline=0, command=redo)
+edit_menu.add_separator()
+edit_menu.add_command(label='Cut', accelerator='Ctrl+X', compound='left', image=cut_icon, underline=0, command=cut)
+edit_menu.add_command(label='Copy', accelerator='Ctrl+C', compound='left', image=copy_icon, underline=0, command=copy)
+edit_menu.add_command(label='Paste', accelerator='Ctrl+V', compound='left', image=paste_icon, underline=0,
+                      command=paste)
+edit_menu.add_separator()
 
-#for view in the menu bar
-ViewMenu=Menu(menu,tearoff=0)
-menu.add_cascade(label="view",menu=ViewMenu)
-ViewMenu.add_command(label="status bar",command=new) 
-ViewMenu.add_command(label="hide menu",command=new)
-ViewMenu.add_command(label="enter full screen",command=new)
-ViewMenu.add_separator()
-ViewMenu.add_command(label="Exit",command=root.destroy)
-
-#for about in menu bar
-aboutMenu=Menu(menu,tearoff=0)
-menu.add_cascade(label="about",menu=aboutMenu)
-aboutMenu.add_command(label="help",command=help)
-aboutMenu.add_separator()
-aboutMenu.add_command(label="Exit",command=root.destroy)
+edit_menu.add_command(label="Exit",command=root.destroy)
+menu_bar.add_cascade(label='Edit', menu=edit_menu)
+# End of Edit Menu
 
 
+view_menu = Menu(menu_bar, tearoff=0)
+edit_menu.add_separator()
+show_line_number = IntVar()
+show_line_number.set(1)
+view_menu.add_checkbutton(label="Show Line Number", variable=show_line_number)
+show_cursor_info = IntVar()
+show_cursor_info.set(1)
+view_menu.add_checkbutton(label='Show Cursor Location at Bottom', variable=show_cursor_info, command=show_cursor)
+view_menu.add_separator()
+view_menu.add_command(label="Exit",command=root.destroy)
+themes_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='View', menu=view_menu)
 
+# start of About Menu
+about_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='About', menu=about_menu)
+about_menu.add_command(label='Help', underline=0, command=display_help)
+about_menu.add_separator()
+about_menu.add_command(label="Exit",command=root.destroy)
+root.config(menu=menu_bar)
+#End of About Menu
+
+# Adding top shortcut bar and left line number bar
+shortcut_bar = Frame(root, height=25)
+shortcut_bar.pack(expand='no', fill='x')
+
+# Adding shortcut icons
+icons = ('new_file', 'open_file', 'save', 'cut', 'copy', 'paste', 'undo', 'redo')
+for i, icon in enumerate(icons):
+    tool_bar_icon = PhotoImage(file='icons/{}.gif'.format(icon)).zoom(2, 2)
+    cmd = eval(icon)
+    tool_bar = Button(shortcut_bar, image=tool_bar_icon, height=35, width=35, command=cmd)
+    tool_bar.image = tool_bar_icon
+    tool_bar.pack(side='left')
+
+line_number_bar = Text(root, width=4, padx=3, takefocus=0, fg='white', border=0, background='#282828', state='disabled',
+                       wrap='none')
+line_number_bar.pack(side='left', fill='y')
+
+# adding the main context Text widget and Scrollbar Widget
+content_text = Text(root, wrap='word')
+content_text.pack(expand='yes', fill='both')
+
+scroll_bar = Scrollbar(content_text)
+content_text.configure(yscrollcommand=scroll_bar.set)
+scroll_bar.config(command=content_text.yview)
+scroll_bar.pack(side='right', fill='y')
+
+# addind cursor info label
+cursor_info_bar = Label(content_text, text='Line: 1 | Column: 1')
+cursor_info_bar.pack(expand='no', fill=None, side='right', anchor='se')
+
+# setting up the pop-up menu
+popup_menu = Menu(content_text)
+for i in ('cut', 'copy', 'paste', 'undo', 'redo'):
+    cmd = eval(i)
+    popup_menu.add_command(label=i, compound='left', command=cmd)
+popup_menu.add_separator()
+content_text.bind('<Button-3>', show_popup_menu)
+
+# Handling binding
+
+content_text.bind('<Control-N>', new_file)
+content_text.bind('<Control-n>', new_file)
+content_text.bind('<Control-O>', open_file)
+content_text.bind('<Control-o>', open_file)
+content_text.bind('<Control-S>', save)
+content_text.bind('<Control-s>', save)
+
+content_text.bind('<Control-Y>', redo)
+content_text.bind('<Control-y>', redo)
+
+content_text.bind('<KeyPress-F1>', display_help)
+
+content_text.bind('<Any-KeyPress>', on_content_changed)
+content_text.tag_configure('active_line', background='ivory2')
+
+content_text.bind('<Button-3>', show_popup_menu)
+content_text.focus_set()
+
+# END OF MENU
+
+root.protocol('WM_DELETE_WINDOW', exit_editor)
+#Root for main window
 root.mainloop()
